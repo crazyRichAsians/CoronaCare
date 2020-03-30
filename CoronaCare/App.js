27b333d350294dd650db4f970c77b2fc18ab13ee
kwokjunn <kwok.zi.jun.2015@vjc.sg>
2020-03-30 15:45:08 +0800

    rishab is the best

--- CoronaCare/App.js
+++ CoronaCare/App.js
@@ -40,14 +40,14 @@ const App: () => React$Node = () => {
           )}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionTitle}>Rishab is the best</Text>
               <Text style={styles.sectionDescription}>
                 Edit <Text style={styles.highlight}>App.js</Text> to change this
                 screen and then come back to see your edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionTitle}>Jerome also not bad</Text>
               <Text style={styles.sectionDescription}>
                 <ReloadInstructions />
               </Text>
