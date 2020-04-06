d9f42572db36b2934a6a06b599379b7fbcc9d3f4
kwokjunn <kwok.zi.jun.2015@vjc.sg>
2020-04-06 21:40:46 +0800

    added a table

--- CoronaCare/src/screens/RecordsScreen.js
+++ CoronaCare/src/screens/RecordsScreen.js
@@ -1,26 +1,58 @@
-import React, {Component} from 'react';
-import {View, TextInput, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
-export default class RecordsScreen extends Component {
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+      HeadTable: ['S/N', 'Day', 'AM', 'PM'],
+      DataTable: [
+        ['1', '2', '3', '4'],
+        ['a', 'b', 'c', 'd'],
+        ['1', '2', '3', '4'],
+        ['a', 'b', 'c', 'd'],
+        ['1', '2', '3', '4']
+      ]
+    }
+  }
+
   render() {
+    const state = this.state;
     return (
-      <View style={styles.outsideWrapper}>
-        <Text style={styles.welcomeText}>Records</Text>
+      
+      <View style={styles.container}>
+        
+        <Table borderStyle={{borderWidth: 1, borderColor: '#000000'}}>
+          <Row data={state.HeadTable} style={styles.HeadStyle} textStyle={styles.TableText}/>
+          <Rows data={state.DataTable} textStyle={styles.TableText}/>
+        </Table>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  outsideWrapper: {
-    backgroundColor: '#FFFFFF',
-    display: 'flex',
-    padding: 30,
+  title: {
+    fontFamily: 'Verdana',
+    fontSize: 30,
+    marginTop: 20,
+    marginBottom: 20
   },
-  welcomeText: {
-    fontWeight: '300',
-    fontSize: 40,
-    color: '#000000',
+  container: { 
+    flex: 1,
+    padding: 20,
+    paddingTop: 30,
+    backgroundColor: '#ffffff' 
   },
+  HeadStyle: { 
+    height: 30,
+    alignContent: "center",
+    backgroundColor: '#6F7BD9'
+  },
+  TableText: { 
+    margin: 10
+  }
 });
-
