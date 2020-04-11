6cfc3faf1d5f2c4518e2e0f6942c7b3ef723962e
DarkArcZ <jeromewongzh@u.nus.edu>
2020-04-11 23:12:24 +0800

    * Added Flat List onto RecordScreen * Status Image and Text can now be displayed on the Flat List

--- CoronaCare/src/screens/RecordsScreen.js
+++ CoronaCare/src/screens/RecordsScreen.js
@@ -1,26 +1,88 @@
 import React, {Component} from 'react';
-import {View, TextInput, Text, StyleSheet} from 'react-native';
+import {View, Text, FlatList, StyleSheet, Image} from 'react-native';
 
 export default class RecordsScreen extends Component {
+  constructor(props) {
+    
+    super(props);
+
+    // this.array = [{
+    //   title: 'ONE'
+    // },
+    // {
+    //   title: 'TWO'
+    // },
+    // {
+    //   title: 'THREE'
+    // },
+    // {
+    //   title: 'FOUR'
+    // },
+    // {
+    //   title: 'FIVE'
+    // }
+    // ],
+
+    // this.state = {
+    //   arrayHolder: [],
+    // }
+  }
+
+  // componentDidMount() {
+  //   this.setState({ arrayHolder: [...this.array] })
+  // }
+
   render() {
+    const DATA = [
+      {title: "jerome", image:"https://reactnative.dev/img/tiny_logo.png", key: "1"},
+      {title: "rishab",  image:"https://reactnative.dev/img/tiny_logo.png",  key: "2" }
+    ]
     return (
-      <View style={styles.outsideWrapper}>
-        <Text style={styles.welcomeText}>Records</Text>
+      <View style={styles.container}>
+        <Text style={styles.welcomeText}>Records</Text> 
+        <FlatList
+          keyExtractor={(item) => item.key}
+          data={DATA}
+          renderItem={({ item }) => (
+            <View style={styles.item}>
+            <Image source = {{uri: item.image}} style={styles.Image}></Image>
+            <Text style={styles.title}> {item.title} </Text>
+            </View> 
+          )}
+
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  outsideWrapper: {
-    backgroundColor: '#FFFFFF',
-    display: 'flex',
-    padding: 30,
+  container: {
+    flex: 1,
+  },
+  Image: {
+    width:50,
+    height:50,
+    margin:7,
+    borderRadius: 7
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '300',
   },
   welcomeText: {
     fontWeight: '300',
     fontSize: 40,
     color: '#000000',
+    justifyContent: 'flex-start',
+    flex: 1,
+    padding: 30,
   },
 });
 
